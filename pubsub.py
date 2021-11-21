a73fd7510d0625078bb460fcd8b4f84974b70036
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2021-11-20 22:12:20 -0700

    Add default Publisher implementation

--- pubsub.py
+++ pubsub.py
@@ -1,12 +1,20 @@
 class Publisher:
+    def __init__(self):
+        self._subs = []
+        
     def add_subscriber(self, subscriber):
-        raise NotImplementedError
+        assert isinstance(subscriber, Subscriber)
+        self._subs.append(subscriber)
 
-    def remove_subscriber(self):
-        raise NotImplementedError
+    def remove_subscriber(self, subscriber=None):
+        if subscriber is None:
+            self._subs.clear()
+        else:
+            self._subs.remove(subscriber)
 
     def publish(self, message):
-        raise NotImplementedError
+        for sub in self._subs:
+            sub.update(message)
 
 
 class Subscriber:
