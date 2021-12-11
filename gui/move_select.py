222ae105ddbd9e5de0ec148902e944d5cd9ca982
Brian Glassman <15279030+BrianGlassman@users.noreply.github.com>
2021-12-10 18:23:33 -0700

    Remove unused code from move_select

--- gui/move_select.py
+++ gui/move_select.py
@@ -55,37 +55,4 @@ class Move_Select(tk.Frame, Publisher):
             
         # Initialize Publisher
         Publisher.__init__(self)
-    
-if __name__ == "__main__":
-    # Hacky code to mess with the path, since this script usually won't be run directly anyway
-    # https://stackoverflow.com/questions/16981921/relative-imports-in-python-3
-    import sys, os
-    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-    PARENT_DIR = os.path.split(SCRIPT_DIR)[0]
-    if PARENT_DIR not in sys.path:
-        sys.path.append(PARENT_DIR)
-    if SCRIPT_DIR not in sys.path:
-        sys.path.append(SCRIPT_DIR)
-    from pokemon import Pokemon
-    from moves.move import move_factory
-    
-    # FIXTURE: create the pokemon
-    pokemon = Pokemon(13)
-    move = move_factory('Earthquake')
-    pokemon.add_move(move)
-    move = move_factory('Rest')
-    pokemon.add_move(move)
-    move = move_factory('Rock Slide')
-    pokemon.add_move(move)
-    move = move_factory('Double Team')
-    pokemon.add_move(move)
-    del move
-    Model.player.add_to_team(pokemon)
-    
-    
-    # FIXTURE: create the base Tkinter window
-    root = util.Default_Window(y_stretch=10/(10+12))
-    util.gridconfigure(root)
-    frame = Move_Select(root)
-    frame.grid(row=0, column=0, sticky='NSEW')
-    root.mainloop()
+        
